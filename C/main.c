57005ce0f3f874479c7219f27c79c1acc886bf70
tormeh <tormod.hellen@gmail.com>
2014-10-03 18:06:46 +0200

    C-stuff

--- C/main.c
+++ C/main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 
-int 
-main(){
+int * intmap(int array[], int arrayLength, int (*function)(int) )
+{
+  int retarray[arrayLength];
+  for(int i = 0; i < arrayLength; i++)
+  {
+    retarray[i] = function(array[i]);
+  }
+  return *retarray;
+}
+
+int square(int x)
+{
+  return x*x;
+}
+
+int main()
+{
   printf("Hello World\n");
+
+  int array[3] = {1,2,3};
+  int *p;
+  int arrayb[3];
+  p = intmap(array, 3, square);
+
+  for(int i=0; i<3; i++)
+  {
+    printf("%d\n" , arrayb[i]);
+  }
+  
 }
