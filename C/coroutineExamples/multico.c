bb17ad4adc31fe3ee4f008e44861b47f977a54ff
Tormod Hellen <tormod.hellen@gmail.com>
2014-11-08 17:45:41 +0100

    added memory to multico

--- C/coroutineExamples/multico.c
+++ C/coroutineExamples/multico.c
@@ -1,40 +1,48 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-static void printfoo()
+struct Datastruct
+{
+  int counter;
+};
+
+static void printfoo(struct Datastruct *ds)
 {
   while(true)
   {
-    printf("foo\n");
+    printf("foo %d\n", ds->counter);
+    ds->counter++;
     return;
   }
 }
 
-static void printbaz()
+static void printbaz(struct Datastruct *ds)
 {
   while(true)
   {
-    printf("baz\n");
+    printf("baz %d\n", ds->counter);
+    ds->counter++;
     return;
   }
 }
 
-static void printlol()
+static void printlol(struct Datastruct *ds)
 {
   while(true)
   {
-    printf("lol\n");
+    printf("lol %d\n", ds->counter);
+    ds->counter++;
     return;
   }
 }
 
-__attribute__ ((noreturn)) static void scheduler(void (*functionPointerArray[])(), int arraylen)
+__attribute__ ((noreturn)) static void scheduler(void (*functionPointerArray[])(struct Datastruct *ds), struct Datastruct *ds, int arraylen)
 {
   while(true)
   {
     for(int i=0; i<arraylen; i++)
     {
-      functionPointerArray[i]();
+      functionPointerArray[i](&ds[i]);
     }
   }
 }
@@ -48,8 +56,13 @@ int main()
   functionPointerArray[0] = printfoo;
   functionPointerArray[1] = printbaz;
   functionPointerArray[2] = printlol;
+  
+  struct Datastruct ds[3];
+  ds[0].counter = 0;
+  ds[1].counter = 0;
+  ds[2].counter = 0;
     
-  scheduler(functionPointerArray, 3);
+  scheduler(functionPointerArray, &ds[0], 3);
     
   //return 0;
 }
